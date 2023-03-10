c8822a91343b52128a843d9343d1fb0e0573e81a
Jéssica Pedro <jessicapedro165@gmail.com>
2023-03-10 20:21:46 +0000

    d085-aula16

--- Modulo3/desafios/d085-aula16.py
+++ Modulo3/desafios/d085-aula16.py
@@ -3,7 +3,55 @@
 # - Uma listagem com as pessoas mais pesadas 
 # - Uma listagem com as pessoas mais leves
 
-pessoas = []
+
+dadosTemp, dadosPessoa = [], []
+maior, menor = 0, 0
+
+while True:
+    dadosTemp.append(input('NOME: ').strip().capitalize())
+    dadosTemp.append(float(input('PESO: ')))
+    print('-' * 30)
+
+    # Maior e menor peso entre os dados informados
+    if len(dadosPessoa) == 0:
+        maior = menor = dadosTemp[1]
+    elif dadosTemp[1] > maior:
+        maior = dadosTemp[1]
+    elif dadosTemp[1] < menor:
+        menor = dadosTemp[1]
+
+    dadosPessoa.append(dadosTemp[:])  # Fazendo uma cópia da lista temporária para principal
+    dadosTemp.clear()  # Limpando a lista temporária
+
+    continuar = input('Continuar? [S/N]: ').strip().upper()
+
+    # Caso usuário informe uma opção inválida
+    while continuar not in ('S', 'N'):
+        print('\nValor incorreto, informe a opção correta...')
+        continuar = input('Continuar? [S/N]: ').strip().upper()
+    print('-' * 30)
+
+    if continuar == 'N':
+        print('Programa finalizado...')
+        print('-' * 30)
+        break
+
+print(f'\nPESSOAS SUBMETIDAS: {len(dadosPessoa)}')
+print(f'MAIOR PESO FOI DE {maior}Kg --> ', end='')
+for pessoa in dadosPessoa:
+    if pessoa[1] == maior:
+        print(f'{pessoa[0]}', end=' ')
+print()
+
+print(f'MENOR PESO FOI DE {menor}Kg --> ', end='')
+for pessoa in dadosPessoa:
+    if pessoa[1] == menor:
+        print(f'{pessoa[0]}', end=' ')
+
+
+
+
+""" pessoas = []
 pessoa = []
 peso = []
 pessoapeso = []
@@ -36,4 +84,4 @@ while True:
 print(f'Informou a lista: {pessoas}')
 print(f'Informou {quant} pessoas')
 print(f'A pesoa mais pesada, pesa {pesada}')
-print(f'A pesoa mais leve, pesa {leve}')
+print(f'A pesoa mais leve, pesa {leve}') """
