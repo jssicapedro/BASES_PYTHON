74e168206f7af169e24673287fd5d202e90a7677
Jéssica Pedro <jessicapedro165@gmail.com>
2023-03-18 14:24:35 +0000

    finalizada aula 16

--- Modulo3/desafios/d089-aula16.py
+++ Modulo3/desafios/d089-aula16.py
@@ -1,2 +1,24 @@
 # Programa que ajude um jogador do totoloto a criar palpites. O programa deve perguntar quantos jogos serão criados e vai sortear 6 números entre 1 e 60 para cada jogo.
-
+from random import randint
+from time import sleep
+lista = []
+jogos = []
+quant = int(input('Quantos jogos quer sortear? '))
+tot = 1
+while tot <= quant:
+    count = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            count += 1
+        if count >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot +=1
+print('='*5, f'Sorteando {quant} Jogos', '='*5)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
--- Modulo3/desafios/d090-aula16.py
+++ Modulo3/desafios/d090-aula16.py
@@ -1,2 +1,26 @@
 # Programa que leia o nome  e 2 notas de vários alunos e guarda tudo numa lista composta. Bi fim mostra um boletim contendo a média de cada um e permita que o utilizador possa mostrar as notas de cada aluno individualmente.
 
+ficha = []
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1+nota2)/2
+    ficha.append([nome, [nota1,nota2], media])
+    resp = str(input('Quer continuar?[S/N] '))
+    if resp in 'nN':
+        break
+print('-='*5, 'Notas', '-='*5)
+print(f'{"N.º":<4}{"Nome":<10}{"Média":<8}')
+print('-'*26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:<8.1f}')
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar as notas de que aluno? (999 interrompe): '))
+    if opc == 999:
+        print('Finalizado...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('Volte sempre...')
